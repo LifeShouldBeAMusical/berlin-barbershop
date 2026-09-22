df14fa42201a2b43088f472dcd8cc9dee629931f
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 01:02:36 +0200

    Fix Test

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest'
-
+import App from '@/App.vue'
 import { mount } from '@vue/test-utils'
-import App from '../App.vue'
+import { describe, expect, it } from 'vitest'
 
 describe('App', () => {
 	it('mounts renders properly', () => {
 		const wrapper = mount(App)
-		expect(wrapper.text()).toContain('You did it!')
+		expect(wrapper.text()).toContain('Berlin Barbershop')
+		expect(wrapper.text()).toContain('Welcome, dear barbershopper, to Berlin!')
 	})
 })
