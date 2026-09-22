e84622ae99abe684ce01d7d2c4b155d7dd69e0bd
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 01:26:51 +0200

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-	<h1>Berlin Barbershop</h1>
-	<p>Welcome, dear barbershopper, to Berlin!</p>
+	<v-app>
+		<v-app-bar flat>
+			<v-toolbar-title>
+				<v-btn href="/">Home</v-btn>
+			</v-toolbar-title>
+		</v-app-bar>
+		<v-container class="router">
+			<h1>Berlin Barbershop</h1>
+			<p>Welcome, dear barbershopper, to Berlin!</p>
+		</v-container>
+	</v-app>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.router {
+	margin: 4em 0 0 0;
+	h1 {
+		text-align: center;
+	}
+}
+</style>
