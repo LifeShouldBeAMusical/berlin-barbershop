45b99850626818e0d464d2407009b8636957ef63
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 02:20:36 +0200

    Home Strings

--- src/component/MenuBar.vue
+++ src/component/MenuBar.vue
@@ -7,7 +7,7 @@ const { t } = useI18n()
 <template>
 	<v-app-bar flat>
 		<v-toolbar-title>
-			<v-btn href="/">Home</v-btn>
+			<v-btn href="/">{{ t('home.menu') }}</v-btn>
 			<v-btn href="/bash">{{ t('bash.menu') }}</v-btn>
 			<v-btn href="/chorus">{{ t('chorus.menu') }}</v-btn>
 			<v-btn href="/event">{{ t('event.menu') }}</v-btn>
