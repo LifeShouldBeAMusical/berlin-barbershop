8bebb9ea64e3db3c02d9aba9681794b951e3f859
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 02:55:35 +0200

    Compactify

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,9 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
-import pluginOxlint from 'eslint-plugin-oxlint'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import skipFormatting from 'eslint-config-prettier/flat'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import pluginVue from 'eslint-plugin-vue'
+import { globalIgnores } from 'eslint/config'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -11,20 +11,14 @@ import skipFormatting from 'eslint-config-prettier/flat'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-	{
-		name: 'app/files-to-lint',
-		files: ['**/*.{vue,ts,mts,tsx}'],
-	},
+	{ name: 'app/files-to-lint', files: ['**/*.{vue,ts,mts,tsx}'] },
 
 	globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
 	...pluginVue.configs['flat/essential'],
 	vueTsConfigs.recommended,
 
-	{
-		...pluginVitest.configs.recommended,
-		files: ['src/**/__tests__/*'],
-	},
+	{ ...pluginVitest.configs.recommended, files: ['src/**/__tests__/*'] },
 
 	...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
