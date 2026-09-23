54ba1d3cfef90e1d1f9b34f47ec09c2482776812
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 02:50:50 +0200

    Run It

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,22 +11,22 @@ import skipFormatting from 'eslint-config-prettier/flat'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{vue,ts,mts,tsx}'],
-  },
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{vue,ts,mts,tsx}'],
+	},
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+	globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  ...pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
+	...pluginVue.configs['flat/essential'],
+	vueTsConfigs.recommended,
 
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
+	{
+		...pluginVitest.configs.recommended,
+		files: ['src/**/__tests__/*'],
+	},
 
-  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+	...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
-  skipFormatting,
+	skipFormatting,
 )
