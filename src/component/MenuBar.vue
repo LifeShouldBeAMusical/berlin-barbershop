5edb5d05e8569b1c1e84352b7fc663a30e7804b9
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-23 02:12:43 +0200

    Chorus Strings

--- src/component/MenuBar.vue
+++ src/component/MenuBar.vue
@@ -9,7 +9,7 @@ const { t } = useI18n()
 		<v-toolbar-title>
 			<v-btn href="/">Home</v-btn>
 			<v-btn href="/bash">{{ t('bash.menu') }}</v-btn>
-			<v-btn href="/chorus">Choruses</v-btn>
+			<v-btn href="/chorus">{{ t('chorus.menu') }}</v-btn>
 			<v-btn href="/event">Events</v-btn>
 		</v-toolbar-title>
 	</v-app-bar>
